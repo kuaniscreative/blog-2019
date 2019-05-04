0c682b68c351f6ab9f320a32b47589d3eddc0b61
Kuanisinajar <superbenben@livemail.tw>
2019-05-04 22:37:58 +0800

    Added scroll init funciton

--- src/js/animations/infiniteScroll.js
+++ src/js/animations/infiniteScroll.js
@@ -2,28 +2,59 @@
 
 $(function() {
   const targets = $(".ani-infiniteScroll_target");
-  const parent = $(".ani-infiniteScroll_wrapper");
-  const testWidth = $(parent)
-    .eq(0)
-    .width();
+  const parents = $(".ani-infiniteScroll_wrapper");
+  const targetList = [];
 
-  // Clone the contents
+  // init the targets
   for (let item of targets) {
-    $(item)
-      .children()
-      .clone()
-      .appendTo(item);
+    targetList.push(initInfiniteScroll(item));
   }
   // Wheel event
   $(window).on("wheel", e => {
     e.preventDefault();
-    let pos = $(parent)
-      .eq(0)
-      .scrollLeft();
-    pos += e.originalEvent.deltaY;
-
-    $(parent)
-      .eq(0)
-      .scrollLeft(pos);
+    // let pos = $(parents)
+    //   .eq(0)
+    //   .scrollLeft();
+    // pos += e.originalEvent.deltaY;
+    // $(parents)
+    //   .eq(0)
+    //   .scrollLeft(pos);
   });
+
+  function getScrollLeft(target) {
+    $(target).scrollLeft();
+  }
+
+  function scrollUpdate(target) {
+    const scrollLeft = getScrollLeft();
+    const cloneWidth =
+      $(".ani-infiniteScroll_target")
+        .eq(0)
+        .width() - originWidth;
+    let scrollWidth = $(target).scrollWidth;
+  }
+
+  function scrollRender(target) {
+    const cloneSize = $(target).width() - originWidth;
+    if (cloneSize + scrollPos > scrollSize) {
+      setScrollTo(1);
+    } else if (scrollPos <= 0) {
+      setScrollTo(scrollSize - cloneSize);
+    }
+  }
+
+  function initInfiniteScroll(target, parent) {
+    const originWidth = $(target).width();
+    $(target)
+      .children()
+      .clone()
+      .appendTo(target);
+    const scrollWidth = $(target).width();
+    return {
+      parent: $(parent),
+      originWidth: originWidth,
+      scrollWidth: scrollWidth,
+      cloneWidth: scrollWidth - originWidth
+    };
+  }
 });
