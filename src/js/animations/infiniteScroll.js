079bad4cd4c8a94e7567a285d8ce6e483f5c7584
Kuanisinajar <superbenben@livemail.tw>
2019-05-05 14:25:20 +0800

    Finished scroll loop

--- src/js/animations/infiniteScroll.js
+++ src/js/animations/infiniteScroll.js
@@ -6,40 +6,71 @@ $(function() {
   const targetList = [];
 
   // init the targets
-  for (let item of targets) {
-    targetList.push(initInfiniteScroll(item));
+  for (var i = 0; i < targets.length; i++) {
+    targetList.push(initInfiniteScroll(targets[i], parents[i]));
   }
   // Wheel event
   $(window).on("wheel", e => {
     e.preventDefault();
-    // let pos = $(parents)
-    //   .eq(0)
-    //   .scrollLeft();
-    // pos += e.originalEvent.deltaY;
-    // $(parents)
-    //   .eq(0)
-    //   .scrollLeft(pos);
+    // $(targets).each(i => {
+    //   console.log(this)
+    //   if (i % 2 === 0) {
+    //     let curPos = $(this.parent).scrollLeft();
+    //     curPos -= e.originalEvent.deltaY;
+    //     $(this.parent).scrollLeft(curPos);
+    //     scrollUpdate(this);
+    //   } else {
+    //     let curPos = $(this.parent).scrollLeft();
+    //     curPos -= e.originalEvent.deltaY;
+    //     $(this.parent).scrollLeft(curPos);
+    //     scrollUpdate(this);
+    //   }
+    // });
+    for (let [index, item] of $(targetList)
+      .toArray()
+      .entries()) {
+      if (index % 2 === 0) {
+        let curPos = $(item.parent).scrollLeft();
+        curPos -= e.originalEvent.deltaY;
+        $(item.parent).scrollLeft(curPos);
+        scrollUpdate(item);
+      } else {
+        let curPos = $(item.parent).scrollLeft();
+        curPos += e.originalEvent.deltaY;
+        $(item.parent).scrollLeft(curPos);
+        scrollUpdate(item);
+      }
+    }
   });
 
-  function getScrollLeft(target) {
-    $(target).scrollLeft();
+  // function getScrollLeft(target) {
+  //   $(target).scrollLeft();
+  // }
+
+  // function scrollUpdate(target) {
+  //   const scrollLeft = getScrollLeft();
+  //   const cloneWidth =
+  //     $(".ani-infiniteScroll_target")
+  //       .eq(0)
+  //       .width() - originWidth;
+  //   let scrollWidth = $(target).scrollWidth;
+  // }
+
+  function setScrollPos(target, pos) {
+    $(target).scrollLeft(pos);
   }
 
   function scrollUpdate(target) {
-    const scrollLeft = getScrollLeft();
-    const cloneWidth =
-      $(".ani-infiniteScroll_target")
-        .eq(0)
-        .width() - originWidth;
-    let scrollWidth = $(target).scrollWidth;
-  }
-
-  function scrollRender(target) {
-    const cloneSize = $(target).width() - originWidth;
-    if (cloneSize + scrollPos > scrollSize) {
-      setScrollTo(1);
-    } else if (scrollPos <= 0) {
-      setScrollTo(scrollSize - cloneSize);
+    const originWidth = target.originWidth;
+    const scrollWidth = target.scrollWidth;
+    const cloneWidth = target.cloneWidth;
+    let scrollLeft = $(target.parent).scrollLeft();
+    if (cloneWidth + scrollLeft >= scrollWidth) {
+      setScrollPos(target.parent, 1);
+    } else if (scrollLeft <= 0) {
+      setScrollPos(target.parent, scrollWidth - cloneWidth);
+    } else {
+      console.log(scrollWidth, cloneWidth, scrollLeft);
     }
   }
 
