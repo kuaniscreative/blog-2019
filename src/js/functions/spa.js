80df3671103022ac90a94acb8d21762b256d8def
Kuanisinajar <superbenben@livemail.tw>
2019-05-16 11:54:26 +0800

    Fixed broken animation in spa

--- src/js/functions/spa.js
+++ src/js/functions/spa.js
@@ -1,3 +1,5 @@
+import { articleAnimationShow } from '../components/articles';
+
 export const requestArticle = param => {
   $.ajax({
     url: `pages/json/${param}.json`,
@@ -16,8 +18,8 @@ export const requestArticle = param => {
       $(".articles_title span").html(res.title);
       $(".articles_title--mobile span").html(res.title);
       $(".articles_content--scroll").html(res.content);
-      console.log($(".articles_title").css("opacity"));
-      $(".articles_title span").addClass('ani-trigger');
+      articleAnimationShow()
+      
       // $(".articles_title span")
       //   .hide()
       //   .show();
